430599807fbb421f8a2661ad26972b8370a01fbf
Cris575 <cg691539@gmail.com>
2026-01-01 16:47:35 -0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,51 @@
+
+import { getUserInfo, $ } from "./module.js";
+
+window.addEventListener('load', () => {
+  getUserInfo()
+    .then((data) => ShowInfo(data))
+    .catch((err) => console.error(err.message));
+})
+
+$('.search-button').addEventListener('click', () => {
+  const username = $('#username').value;
+  if (username !== "") {
+    getUserInfo(username)
+      .then((data) => ShowInfo(data))
+      .catch((err) => console.error(err.message));
+  } else {
+    alert("Please enter a username");
+  }
+
+});
+
+function ShowInfo(user) {
+
+  const date = new Date(user.created_at);
+
+  const objDate = {
+    day: date.getDate(),
+    month: date.getMonth(),
+    year: date.getFullYear()
+  }
+
+  $('#user-avatar').src = user.avatar_url;
+  $('#user-login').textContent = user.login;
+  $('#user-name').textContent = user.name;
+  $('#user-location').textContent = user.location ?? "Location not available";
+  $('#user-company').textContent = user.company ?? "Company not available";
+  $('#user-blog').textContent = user.blog;
+  $('#user-twitter').textContent = user.twitter_username ?? "Not available";
+  $('#user-date').textContent = `Joined ${objDate.day} ${getMonth(objDate.month)} ${objDate.year}`;
+  $('#user-bio').textContent = user.bio ?? "This profile has no bio";
+  $('#user-repos').textContent = user.public_repos;
+  $('#user-followers').textContent = user.followers;
+  $('#user-following').textContent = user.following;
+}
+
+
+
+function getMonth(month) {
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  return months[month];
+}
---
+++ src/module.js
@@ -0,0 +1,24 @@
+var options = {
+    method: 'GET',
+    headers: {
+        Accept: 'application/vnd.github+json',
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+        'x-GitHub-Api-Version': '2022-11-28'
+    }
+}
+//octocat
+
+export async function getUserInfo(username = "octocat") {
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}`, options)
+        return await response.json();
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+export function $(selector) {
+    return document.querySelector(selector)
+}
+
+
